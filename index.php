21fa3a802a6a3e3d72e727da27219299c71a43ad
Leonardo Mendes <leo.mi.me@gmail.com>
2016-03-08 22:43:52 -0300

    Performance

--- index.php
+++ index.php
@@ -22,133 +22,7 @@
     </head>
 
     <body>
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript" src="js/materialize.min.js"></script>
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $('select').material_select();
-                $('.modal-trigger').leanModal();
-                $('#errolog').hide();
-                $('#erroreg').hide(); //Esconde o elemento com id errolog
-                $('#prog_log').hide(); //Esconde o elemento com id errolog
-                $('#prog_reg').hide(); //Esconde o elemento com id errolog
-                $('#materia').hide();
-                $('#log').hide();
-                $('#logm').hide();
-                $('#login').hide();
-                $('#register').hide();
-                $('#logo-container').click(function () {
-                    $('#login').hide();
-                    $('#register').hide();
-                    $('#principal').show();
-                    $('#footer').removeClass('fixa');
-                    $('#footer2').removeClass('fixa1');
-                });
-                $('#troca_p_registro').click(function () {
-                    $('#login').hide();
-                    $('#register').show();
-                    $('#footer').removeClass('fixa');
-                    $('#footer2').removeClass('fixa1');
-                });
-                $('#troca_p_login').click(function () {
-                    $('#login').show();
-                    $('#register').hide();
-                    $('#footer').addClass('fixa');
-                    $('#footer2').addClass('fixa1');
-                });
-                //comece agora
-                $('#abrelogin').click(function () {
-                    $('#principal').hide();
-                    $('#footer').addClass('fixa');
-                    $('#footer2').addClass('fixa1');
-                    $('#login').show();
-                    $('#register').hide();
-                });
-                //area
-                $('#abrelogin1').click(function () {
-                    $('#principal').hide();
-                    $('#footer').addClass('fixa');
-                    $('#footer2').addClass('fixa1');
-                    $('#login').show();
-                    $('#register').hide();
-                });
-                //sidenav
-                $('#abrelogin2').click(function () {
-                    $('.button-collapse').sideNav('hide');
-                    $('#principal').hide();
-                    $('#footer').addClass('fixa');
-                    $('#footer2').addClass('fixa1');
-                    $('#login').show();
-                    $('#register').hide();
-                });
-                var islogged = <?=$islogged?>;
-                if (islogged == 1) {
-                    $('#principal').hide();
-                    $('#materia').show();
-                    $('#nlog').hide();
-                    $('#nlogm').hide();
-                    $('#log').show();
-                    $('#logm').show();
-                }
-                $('#formlogin').submit(function () { //Ao submeter formulário
-                    $('#prog_log').show();
-                    var email = $('#email').val(); //Pega valor do campo email
-                    var senha = $('#senha').val(); //Pega valor do campo senha
-                    $.ajax({ //Função AJAX
-                        url: "login.php", //Arquivo php
-                        type: "post", //Método de envio
-                        data: "email=" + email + "&senha=" + senha, //Dados
-                        success: function (result) { //Sucesso no AJAX
-                            if (result == 1) {
-                                //Redireciona
-                                $('#login').hide();
-                                $('#materia').show();
-                                $('#nlog').hide();
-                                $('#log').show();
-                                $('#footer').removeClass('fixa');
-                                $('#footer2').removeClass('fixa1');
-                            } else {
-                                $('#errolog').show(); //Informa o erro
-                                $('#prog_log').hide();
-                            }
-                        }
-                    })
-                    return false; //Evita que a página seja atualizada
-                })
-                $('#formregister').submit(function () { //Ao submeter formulário
-                    $('#prog_reg').show();
-                    var nome = $('#rnome').val(); //Pega valor do campo email
-                    var email = $('#remail').val(); //Pega valor do campo senha
-                    var senha = $('#rsenha').val(); //Pega valor do campo senha
-                    var csenha = $('#rcsenha').val(); //Pega valor do campo senha
-                    var ano = $('#rano').val(); //Pega valor do campo senha
-                    var code = $('#rcode').val(); //Pega valor do campo senha
-                    $.ajax({ //Função AJAX
-                        url: "register.php", //Arquivo php
-                        type: "post", //Método de envio
-                        data: "nome=" + nome + "&senha=" + senha + "&email=" + email + "&csenha=" + csenha + "&ano=" + ano + "&code=" + code,
-                        success: function (result) { //Sucesso no AJAX
-                            if (result == 1) {
-                                //Redireciona
-                                $('prog_reg').hide();
-                                $('#register').hide();
-                                $('#login').show();
-                            } else if (result == 3) {
-                                $('#prog_reg').hide();
-                                $('#erroreg').html("As senhas não coincidem!");
-                                $('#erroreg').show();
-                            } else if (result == 2) {
-                                $('#erroreg').html('Este e-mail já está em uso!');
-                                $('#erroreg').show();
-                                $('#prog_reg').hide();
-                            }
-                        }
-                    })
-                    return false; //Evita que a página seja atualizada
-                })
 
-            });
-        </script>
         <!-- Menu -->
         <?= $nav ?>
             <div class="row container" id="login">
@@ -413,8 +287,137 @@
             <?= $footerindex ?>
 
                 <!--  Scripts-->
-                <script src="js/materialize.js"></script>
-                <script src="js/init.js"></script>
+                <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+                <script type="text/javascript" src="js/materialize.min.js"></script>
+                <script type="text/javascript">
+                    $(document).ready(function () {
+                        $(function () {
+                            $('.button-collapse').sideNav();
+                            $('.parallax').parallax();
+                        });
+                        $('select').material_select();
+                        $('.modal-trigger').leanModal();
+                        $('#errolog').hide();
+                        $('#erroreg').hide(); //Esconde o elemento com id errolog
+                        $('#prog_log').hide(); //Esconde o elemento com id errolog
+                        $('#prog_reg').hide(); //Esconde o elemento com id errolog
+                        $('#materia').hide();
+                        $('#log').hide();
+                        $('#logm').hide();
+                        $('#login').hide();
+                        $('#register').hide();
+                        $('#logo-container').click(function () {
+                            $('#login').hide();
+                            $('#register').hide();
+                            $('#principal').show();
+                            $('#footer').removeClass('fixa');
+                            $('#footer2').removeClass('fixa1');
+                        });
+                        $('#troca_p_registro').click(function () {
+                            $('#login').hide();
+                            $('#register').show();
+                            $('#footer').removeClass('fixa');
+                            $('#footer2').removeClass('fixa1');
+                        });
+                        $('#troca_p_login').click(function () {
+                            $('#login').show();
+                            $('#register').hide();
+                            $('#footer').addClass('fixa');
+                            $('#footer2').addClass('fixa1');
+                        });
+                        //comece agora
+                        $('#abrelogin').click(function () {
+                            $('#principal').hide();
+                            $('#footer').addClass('fixa');
+                            $('#footer2').addClass('fixa1');
+                            $('#login').show();
+                            $('#register').hide();
+                        });
+                        //area
+                        $('#abrelogin1').click(function () {
+                            $('#principal').hide();
+                            $('#footer').addClass('fixa');
+                            $('#footer2').addClass('fixa1');
+                            $('#login').show();
+                            $('#register').hide();
+                        });
+                        //sidenav
+                        $('#abrelogin2').click(function () {
+                            $('.button-collapse').sideNav('hide');
+                            $('#principal').hide();
+                            $('#footer').addClass('fixa');
+                            $('#footer2').addClass('fixa1');
+                            $('#login').show();
+                            $('#register').hide();
+                        });
+                        var islogged = <?=$islogged?>;
+                        if (islogged == 1) {
+                            $('#principal').hide();
+                            $('#materia').show();
+                            $('#nlog').hide();
+                            $('#nlogm').hide();
+                            $('#log').show();
+                            $('#logm').show();
+                        }
+                        $('#formlogin').submit(function () { //Ao submeter formulário
+                            $('#prog_log').show();
+                            var email = $('#email').val(); //Pega valor do campo email
+                            var senha = $('#senha').val(); //Pega valor do campo senha
+                            $.ajax({ //Função AJAX
+                                url: "login.php", //Arquivo php
+                                type: "post", //Método de envio
+                                data: "email=" + email + "&senha=" + senha, //Dados
+                                success: function (result) { //Sucesso no AJAX
+                                    if (result == 1) {
+                                        //Redireciona
+                                        $('#login').hide();
+                                        $('#materia').show();
+                                        $('#nlog').hide();
+                                        $('#log').show();
+                                        $('#footer').removeClass('fixa');
+                                        $('#footer2').removeClass('fixa1');
+                                    } else {
+                                        $('#errolog').show(); //Informa o erro
+                                        $('#prog_log').hide();
+                                    }
+                                }
+                            })
+                            return false; //Evita que a página seja atualizada
+                        })
+                        $('#formregister').submit(function () { //Ao submeter formulário
+                            $('#prog_reg').show();
+                            var nome = $('#rnome').val(); //Pega valor do campo email
+                            var email = $('#remail').val(); //Pega valor do campo senha
+                            var senha = $('#rsenha').val(); //Pega valor do campo senha
+                            var csenha = $('#rcsenha').val(); //Pega valor do campo senha
+                            var ano = $('#rano').val(); //Pega valor do campo senha
+                            var code = $('#rcode').val(); //Pega valor do campo senha
+                            $.ajax({ //Função AJAX
+                                url: "register.php", //Arquivo php
+                                type: "post", //Método de envio
+                                data: "nome=" + nome + "&senha=" + senha + "&email=" + email + "&csenha=" + csenha + "&ano=" + ano + "&code=" + code,
+                                success: function (result) { //Sucesso no AJAX
+                                    if (result == 1) {
+                                        //Redireciona
+                                        $('prog_reg').hide();
+                                        $('#register').hide();
+                                        $('#login').show();
+                                    } else if (result == 3) {
+                                        $('#prog_reg').hide();
+                                        $('#erroreg').html("As senhas não coincidem!");
+                                        $('#erroreg').show();
+                                    } else if (result == 2) {
+                                        $('#erroreg').html('Este e-mail já está em uso!');
+                                        $('#erroreg').show();
+                                        $('#prog_reg').hide();
+                                    }
+                                }
+                            })
+                            return false; //Evita que a página seja atualizada
+                        })
+
+                    });
+                </script>
 
     </body>
 
